6012ee39d1fe528d00ac98e63cc4ca4a4c368657
Abhishek Bhadange <abhishekbhadange@gmail.com>
2014-07-12 00:04:48 -0700

    Version 1.0

---
+++ Stack/src/StackApp.java
@@ -0,0 +1,20 @@
+
+public class StackApp {
+	
+	public static void main(String args[]) {
+		
+		StackImp si = new StackImp(10);
+		si.push(10);
+		si.push(20);
+		si.push(30);
+		si.push(40);
+		
+		while(!si.isEmpty()) {
+			int value = si.pop();
+			System.out.print(value);
+			System.out.print(" ");
+		}
+		System.out.println("");
+	}
+}
+
