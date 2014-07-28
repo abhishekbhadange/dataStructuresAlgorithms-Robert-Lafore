37e31f48614fc57cef046ac741deadbeaa1f487f
Abhishek Bhadange <abhishekbhadange@gmail.com>
2014-07-27 18:52:15 -0700

    Version 1.0

---
+++ bubbleSort/src/bubbleSort/ArrayBub.java
@@ -0,0 +1,35 @@
+package bubbleSort;
+
+public class ArrayBub {
+	
+	private int[] arr;
+	private int nItems;
+	
+	public ArrayBub(int maxSize) {
+		arr = new int[maxSize];
+		nItems = 0;
+	}
+	public void insert(int data) {
+		arr[nItems++] = data;
+	}
+	public int size() {
+		return nItems;
+	}
+	public void display() {
+		for(int i = 0; i < size(); i++)
+			System.out.print(arr[i] + " ");
+		System.out.println("");
+	}
+	public void bubbleSort() {
+		int in, out, temp;
+		for(out = nItems - 1; out > 0; out--) {
+			for(in = 0; in < out; in++) {
+				if(arr[in] > arr[in + 1]) {
+					temp = arr[in];
+					arr[in] = arr[in + 1];
+					arr[in + 1] = temp;
+				}
+			}
+		}
+	}
+}
